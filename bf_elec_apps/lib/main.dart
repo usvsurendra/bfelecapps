0d742d3f52ae228a8a8bb9e4ab5f00c00bd29554
Surendra <surendra@example.com>
2026-07-16 20:05:30 +0530

    Fix web blank screen: serve CanvasKit locally instead of gstatic CDN

--- bf_elec_apps/lib/main.dart
+++ bf_elec_apps/lib/main.dart
@@ -2,10 +2,10 @@ import 'package:bf_elec_apps/core/theme/app_theme.dart';
 import 'package:bf_elec_apps/features/auth/presentation/pages/splash_page.dart'; 
 import 'package:flutter/material.dart'; 
  
-void main() { 
-  WidgetsFlutterBinding.ensureInitialized(); 
-  runApp(const BfElecApps()); 
-} 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const BfElecApps());
+}
  
 class BfElecApps extends StatelessWidget { 
   const BfElecApps({super.key}); 
